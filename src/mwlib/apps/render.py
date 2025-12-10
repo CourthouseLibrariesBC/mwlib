42c64aafea4d7c6aa4119b087a8a2d1d748d2ed1
Clicklaw Wikibooks <wikisupport@clicklaw.bc.ca>
2025-12-09 16:50:39 -0800

    Resolves CLWB-949 TOC rendering issue.

--- src/mwlib/apps/render.py
+++ src/mwlib/apps/render.py
@@ -428,4 +428,12 @@ def list_writers():
 
 
 if __name__ == "__main__":
+    import logging, sys
+    root = logging.getLogger()
+    root.handlers[:] = []
+    h = logging.StreamHandler(sys.stdout)
+    h.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s'))
+    root.addHandler(h)
+    root.setLevel(logging.INFO)
+
     main()
